c16468dee7f613c556c4b915c359360dda5ceb5b
Carlo Mendoza <carlomendoza89@gmail.com>
2019-07-02 16:48:40 -0700

    removed

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import Firebase from 'firebase'
 require('firebase/firestore')
 
 var firebaseApp = Firebase.initializeApp({
-    apiKey: "AIzaSyAGCRkAJLWYXyVFxRAude6SaQxy6SSTMKQ",
+    
     authDomain: "form-e44cd.firebaseapp.com",
     databaseURL: "https://form-e44cd.firebaseio.com",
     projectId: "form-e44cd",
